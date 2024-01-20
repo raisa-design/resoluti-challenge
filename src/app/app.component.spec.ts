c70f9ac4f5e339d83727a829e30eedf80ac08bab
Raisa_Dev <73198136+raisa-design@users.noreply.github.com>
2024-01-19 21:56:58 -0300

    feat: init project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, resoluti-front');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, resoluti-front'
+    );
   });
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { PfRegisterComponent } from './pages/pf-register/pf-register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'pf-register', component: PfRegisterComponent}
+];
