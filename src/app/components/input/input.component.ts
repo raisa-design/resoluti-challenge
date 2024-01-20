79299cb99a0ff3f5baa382775ec6362c0d1e493d
Raisa_Dev <73198136+raisa-design@users.noreply.github.com>
2024-01-19 21:58:05 -0300

    feat: create base input component

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css'
+})
+export class InputComponent {
+  @Input({ required: true }) label!: string;
+  @Input({ required: true }) name!: string;
+  @Input({ required: false }) type?: string = 'text';
+  @Input({ required: false }) placeholder?: string = '';
+}
